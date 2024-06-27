f26a160c30cb44c5cec21cc1c4c95f8c60056e99
paul-b-b64 <belousko@inbox.ru>
2024-06-27 18:21:32 +0300

    Хард-задание

--- modul2hard.py
+++ modul2hard.py
@@ -1,39 +1,35 @@
 num_ = int(input('Введи число: '))
-deliteli_ = []
-for i in range(1, num_ + 1):
-    # цикл нахождения делителей введенного числа
-    if (num_ % i) == 0 and i > 2:
-        deliteli_.append((i))
-#for i in range(len(deliteli_)): # на каждый делитель ищем пары сумм
-    num_ = deliteli_[i]
-    pass_ = []
-    if num_ % 2 != 0:  # для нечетных делителей так
-        for j in range (1, num_//2 + 1):
-            a = num_ - j
-            pass_.append(j)
-            pass_.append(a)
-    else:
-        for j in range (1, num_//2): # для четных делителей
-            a = num_ - j
-            pass_.append(j)
-            pass_.append(a)
+pass_ = ""
+for i in range(1, num_//2 + 1):
+    for j in range(1, 20):
+        prom_zn = ""
+        if num_ % (i + j) == 0 and i != j:
+            prom_zn += str(i)
+            prom_zn += str(j)
+            if prom_zn not in pass_[::-1]:
+                pass_ += prom_zn
+            elif len(prom_zn) > 2:
+                pass_ += prom_zn
+            elif prom_zn not in pass_:
+                pass_ += prom_zn
 print(pass_)
 
 
 
 
-# for i in range(len(deliteli_)): # на каждый делитель ищем пары сумм
-#     num_ = deliteli_[i]
-#     pass_ = []
-#     if num_ % 2 != 0:  # для нечетных делителей так
-#         for j in range (1, num_//2 + 1):
-#             a = num_ - j
-#             pass_.append(j)
-#             pass_.append(a)
-#         print(pass_)
-#     else:
-#         for j in range (1, num_//2): # для четных делителей
-#             a = num_ - j
-#             pass_.append(j)
-#             pass_.append(a)
-#         print(pass_)
+
+
+# num_ = int(input('Введи число: '))
+# pass_ = []
+# prom_zn = []
+# for i in range(1, num_//2 + 1):
+#     for j in range(2, 20):
+#         if num_ % (i + j) == 0 and i != j:
+#             prom_zn.append(i)
+#             prom_zn.append(j)
+#
+
+
+# b = a[::-1]
+# print(pass_)
+
