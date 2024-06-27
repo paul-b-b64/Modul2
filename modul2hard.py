04bb88e174c3453d1a908a92cb82ada8a59b9c94
paul-b-b64 <belousko@inbox.ru>
2024-06-27 18:22:39 +0300

    Хард-задание

--- modul2hard.py
+++ modul2hard.py
@@ -19,17 +19,3 @@ print(pass_)
 
 
 
-# num_ = int(input('Введи число: '))
-# pass_ = []
-# prom_zn = []
-# for i in range(1, num_//2 + 1):
-#     for j in range(2, 20):
-#         if num_ % (i + j) == 0 and i != j:
-#             prom_zn.append(i)
-#             prom_zn.append(j)
-#
-
-
-# b = a[::-1]
-# print(pass_)
-
